48788009d9b0e3aa28b867065592eb4508615c4b
frc195programming <frc195programming>
2022-03-02 01:45:06 +0000

    modified intake

--- src/intake_node.cpp
+++ src/intake_node.cpp
@@ -9,6 +9,10 @@
 #include "rio_control_node/Robot_Status.h"
 #include "rio_control_node/Motor_Status.h"
 #include "rio_control_node/Motor_Info.h"
+
+#include "intake_node/Intake_Control.h"
+#include "intake_node/Intake_Status.h"
+
 #include "hmi_agent_node/HMI_Signals.h"
 #include "ck_utilities/Piston.hpp"
 
@@ -20,23 +24,22 @@
 #define FRONT_PISTON_ID 1
 #define BACK_PISTON_ID 2
 
-
 #define PIXY_SIGNAL_CAN_ID 11
 
-ros::NodeHandle* node;
+ros::NodeHandle *node;
 
 static ros::Subscriber hmi_subscriber;
 static ros::Subscriber robot_state_subscriber;
 static ros::Subscriber motor_status_subscriber;
 
-static Motor * front_roller;
-static Motor * back_roller;
-static Motor * front_belt;
-static Motor * back_belt;
-static Motor * uptake;
+static Motor *front_roller;
+static Motor *back_roller;
+static Motor *front_belt;
+static Motor *back_belt;
+static Motor *uptake;
 
-static Piston * front_intake_solenoid;
-static Piston * back_intake_solenoid;
+static Piston *front_intake_solenoid;
+static Piston *back_intake_solenoid;
 
 static std::map<uint16_t, rio_control_node::Motor_Info> motor_status_map;
 
@@ -86,10 +89,10 @@ static bool uptake_complete = false;
 static bool eject_complete = false;
 static ros::Time time_roller_last_active = ros::Time(0);
 
-void hmiSignalCallback(const hmi_agent_node::HMI_Signals& msg)
+void hmiSignalCallback(const hmi_agent_node::HMI_Signals &msg)
 {
 	intake_rollers = msg.intake_rollers;
-	if ( intake_rollers )
+	if (intake_rollers)
 	{
 		time_roller_last_active = ros::Time::now();
 	}
@@ -97,151 +100,197 @@ void hmiSignalCallback(const hmi_agent_node::HMI_Signals& msg)
 	manual_intake = msg.manual_intake;
 	manual_outake = msg.manual_outake;
 	drivetrain_fwd_back = msg.drivetrain_fwd_back;
-} 
+}
 
+bool command_shoot = false;
+void intake_control_callback(const intake_node::Intake_Control &msg)
+{
+	(void)msg;
+	command_shoot = msg.command_shoot;
+}
+
+static bool has_a_ball = false;
+static bool uptake_reverse = false;
+static bool uptake_normal = false;
+static bool uptake_shoot = false;
 void stateMachineStep()
 {
 	static ros::Time time_state_entered = ros::Time::now();
-	if ( intake_state != next_intake_state)
+	if (intake_state != next_intake_state)
 	{
 		time_state_entered = ros::Time::now();
 	}
 	ros::Duration time_in_state = ros::Time::now() - time_state_entered;
 	intake_state = next_intake_state;
 
-	switch (intake_state)
+	static ros::Publisher intakeStatusPublisher = node->advertise<intake_node::Intake_Status>("/IntakeStatus", 1);
+	intake_node::Intake_Status statusMsg;
+	if (command_shoot)
 	{
-		case IntakeStates::IDLE:
-		{
-			//Turn Off Belts
-			front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			//Turn Off Rollers
-			front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-		}
-		break;
-
-		case IntakeStates::INTAKE_ROLLERS:
-		{
-			if(deployed_direction == DeployedDirection::FRONT)
-			{
-				front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-				back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			}
-			else
-			{
-				back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-				front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			}
-		}
-		break;
-
-		case IntakeStates::UPTAKE_BALL:
-		{
-			//Put Ball Into Uptake
-			uptake->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-			front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
-			uptake_complete = time_in_state > ros::Duration(0.5);
-		}
-		break;
-
-		case IntakeStates::EJECT_BALL:
-		{
-			//Lob The Ball Out
-			if(deployed_direction == DeployedDirection::FRONT)
-			{
-				front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
-				back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
-			}
-			else
-			{
-				back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
-				front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
-				front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
-			}
-			eject_complete = time_in_state > ros::Duration(0.5);
-		}
-		break;
+		statusMsg.readyToShoot = true;
+	}
+	else
+	{
+		statusMsg.readyToShoot = false;
 	}
 
+	intakeStatusPublisher.publish(statusMsg);
 
 	switch (intake_state)
 	{
-		case IntakeStates::IDLE:
-		{
-			if (intake_rollers)
-			{
-				next_intake_state = IntakeStates::INTAKE_ROLLERS;
-			}
-			else
-			{
-				next_intake_state = IntakeStates::IDLE;
-			}
-		}
-		break;
+	case IntakeStates::IDLE:
+	{
+		//Turn Off Belts
+		front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		//Turn Off Rollers
+		front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+	}
+	break;
 
-		case IntakeStates::INTAKE_ROLLERS:
+	case IntakeStates::INTAKE_ROLLERS:
+	{
+		if (deployed_direction == DeployedDirection::FRONT)
 		{
-			if ( (alliance == Alliance::RED && red_ball_present)
-				|| (alliance == Alliance::BLUE && blue_ball_present) )
-			{
-				next_intake_state = IntakeStates::UPTAKE_BALL;
-			}
-			else if ( (alliance == Alliance::RED && blue_ball_present)
-					|| (alliance == Alliance::BLUE && red_ball_present) )
-			{
-				next_intake_state = IntakeStates::EJECT_BALL;
-			}
-			else if ( intake_rollers || ros::Time::now() - time_roller_last_active < ros::Duration(1) )
-			{
-				next_intake_state = IntakeStates::INTAKE_ROLLERS;
-			}
-			else
-			{
-				next_intake_state = IntakeStates::IDLE;
-			}
+			front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+			back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
 		}
-		break;
-
-		case IntakeStates::UPTAKE_BALL:
+		else
 		{
-			if ( uptake_complete )
-			{
-				next_intake_state = IntakeStates::INTAKE_ROLLERS;
-				uptake_complete = false;
-			}
-			else
-			{
-				next_intake_state = IntakeStates::UPTAKE_BALL;
-			}
+			back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+			front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
 		}
-		break;
+	}
+	break;
 
-		case IntakeStates::EJECT_BALL:
+	case IntakeStates::UPTAKE_BALL:
+	{
+		//Put Ball Into Uptake
+		uptake_normal = true;
+		front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
+		uptake_complete = time_in_state > ros::Duration(0.5);
+	}
+	break;
+
+	case IntakeStates::EJECT_BALL:
+	{
+		//Lob The Ball Out
+		if (deployed_direction == DeployedDirection::FRONT)
 		{
-			if ( eject_complete )
-			{
-				next_intake_state = IntakeStates::INTAKE_ROLLERS;
-				eject_complete = false;
-			}
-			else
-			{
-				next_intake_state = IntakeStates::EJECT_BALL;
-			}	 
+			front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
+			back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
 		}
-		break;
+		else
+		{
+			back_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+			front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
+			front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
+		}
+		eject_complete = time_in_state > ros::Duration(0.5);
+	}
+	break;
+	}
+	switch (intake_state)
+	{
+	case IntakeStates::IDLE:
+	{
+		if (intake_rollers)
+		{
+			next_intake_state = IntakeStates::INTAKE_ROLLERS;
+		}
+		else
+		{
+			next_intake_state = IntakeStates::IDLE;
+		}
+	}
+	break;
 
+	case IntakeStates::INTAKE_ROLLERS:
+	{
+		if ((alliance == Alliance::RED && red_ball_present) || (alliance == Alliance::BLUE && blue_ball_present))
+		{
+			next_intake_state = IntakeStates::UPTAKE_BALL;
+		}
+		else if ((alliance == Alliance::RED && blue_ball_present) || (alliance == Alliance::BLUE && red_ball_present))
+		{
+			next_intake_state = IntakeStates::EJECT_BALL;
+		}
+		else if (intake_rollers || ros::Time::now() - time_roller_last_active < ros::Duration(1))
+		{
+			next_intake_state = IntakeStates::INTAKE_ROLLERS;
+		}
+		else
+		{
+			next_intake_state = IntakeStates::IDLE;
+		}
+	}
+	break;
+
+	case IntakeStates::UPTAKE_BALL:
+	{
+		if (uptake_complete)
+		{
+			next_intake_state = IntakeStates::INTAKE_ROLLERS;
+			has_a_ball = true;
+			uptake_complete = false;
+		}
+		else
+		{
+			next_intake_state = IntakeStates::UPTAKE_BALL;
+		}
+	}
+	break;
+
+	case IntakeStates::EJECT_BALL:
+	{
+		if (eject_complete)
+		{
+			next_intake_state = IntakeStates::INTAKE_ROLLERS;
+			eject_complete = false;
+		}
+		else
+		{
+			next_intake_state = IntakeStates::EJECT_BALL;
+		}
+	}
+	break;
+	}
+	static ros::Time time_command_started = ros::Time::now();
+	if (command_shoot)
+	{
+		uptake_shoot = true;
+		if ((ros::Time::now() - time_command_started) > ros::Duration(0.75))
+		{
+			has_a_ball = false;
+		}
+	}
+	else
+	{
+		time_command_started = ros::Time::now();
+	}
+
+	if (uptake_reverse)
+	{
+		uptake->set(Motor::Control_Mode::PERCENT_OUTPUT, -0.5, 0);
+	}
+	else if (uptake_normal || uptake_shoot)
+	{
+		uptake->set(Motor::Control_Mode::PERCENT_OUTPUT, 0.5, 0);
+	}
+	else
+	{
+		uptake->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
 	}
 }
 
@@ -250,37 +299,37 @@ void determineDeployDirection()
 	constexpr float accumulator_cap = 50;
 	constexpr float threshold = 20;
 	//Figure out deployment direction
-	if(fabs(drivetrain_fwd_back) > 0.1)
+	if (fabs(drivetrain_fwd_back) > 0.1)
 	{
 		static float drivetrain_accumulator = 0;
 		drivetrain_accumulator += drivetrain_fwd_back;
-		if(drivetrain_accumulator > accumulator_cap)
+		if (drivetrain_accumulator > accumulator_cap)
 		{
 			drivetrain_accumulator = accumulator_cap;
 		}
-		if(drivetrain_accumulator < -accumulator_cap)
+		if (drivetrain_accumulator < -accumulator_cap)
 		{
 			drivetrain_accumulator = -accumulator_cap;
 		}
-		if(drivetrain_accumulator > threshold)
+		if (drivetrain_accumulator > threshold)
 		{
 			deployed_direction = DeployedDirection::FRONT;
 		}
-		if(drivetrain_accumulator < -threshold)
+		if (drivetrain_accumulator < -threshold)
 		{
 			deployed_direction = DeployedDirection::BACK;
 		}
 	}
 }
 
-void motorStatusCallback(const rio_control_node::Motor_Status& msg)
+void motorStatusCallback(const rio_control_node::Motor_Status &msg)
 {
-	for ( const rio_control_node::Motor_Info& motorInfo : msg.motors )
+	for (const rio_control_node::Motor_Info &motorInfo : msg.motors)
 	{
 		motor_status_map[motorInfo.id] = motorInfo;
 	}
 
-	if ( motor_status_map.count(PIXY_SIGNAL_CAN_ID) )
+	if (motor_status_map.count(PIXY_SIGNAL_CAN_ID))
 	{
 		red_ball_present = motor_status_map[PIXY_SIGNAL_CAN_ID].forward_limit_closed;
 		blue_ball_present = motor_status_map[PIXY_SIGNAL_CAN_ID].reverse_limit_closed;
@@ -292,24 +341,19 @@ void motorStatusCallback(const rio_control_node::Motor_Status& msg)
 	}
 }
 
-
-
-
-void robotStatusCallback(const rio_control_node::Robot_Status& msg)
+void robotStatusCallback(const rio_control_node::Robot_Status &msg)
 {
 
-	static std::map<int8_t, RobotState> robotStateLookupMap = 
-	{
-		{ rio_control_node::Robot_Status::AUTONOMOUS, RobotState::AUTONOMUS },
-		{ rio_control_node::Robot_Status::DISABLED, RobotState::DISABLED },
-		{ rio_control_node::Robot_Status::TELEOP, RobotState::TELEOP }
-	};
+	static std::map<int8_t, RobotState> robotStateLookupMap =
+		{
+			{rio_control_node::Robot_Status::AUTONOMOUS, RobotState::AUTONOMUS},
+			{rio_control_node::Robot_Status::DISABLED, RobotState::DISABLED},
+			{rio_control_node::Robot_Status::TELEOP, RobotState::TELEOP}};
 
-	static std::map<int8_t, Alliance> allianceLookupMap = 
-	{
-		{ rio_control_node::Robot_Status::RED, Alliance::RED },
-		{ rio_control_node::Robot_Status::BLUE, Alliance::BLUE }
-	};
+	static std::map<int8_t, Alliance> allianceLookupMap =
+		{
+			{rio_control_node::Robot_Status::RED, Alliance::RED},
+			{rio_control_node::Robot_Status::BLUE, Alliance::BLUE}};
 
 	robot_state = robotStateLookupMap[msg.robot_state];
 	alliance = allianceLookupMap[msg.alliance];
@@ -328,7 +372,7 @@ void motorConfiguration(void)
 	front_belt = new Motor(FRONT_BELT_CAN_ID, Motor::Motor_Type::TALON_FX);
 	front_belt->config().set_supply_current_limit(true, 10, 0, 0);
 	front_belt->config().apply();
-	
+
 	back_belt = new Motor(BACK_BELT_CAN_ID, Motor::Motor_Type::TALON_FX);
 	back_belt->config().set_supply_current_limit(true, 10, 0, 0);
 	back_belt->config().apply();
@@ -338,7 +382,7 @@ void motorConfiguration(void)
 	uptake->config().set_forward_limit_switch(MotorConfig::LimitSwitchSource::Deactivated, MotorConfig::LimitSwitchNormal::Disabled);
 	uptake->config().set_reverse_limit_switch(MotorConfig::LimitSwitchSource::Deactivated, MotorConfig::LimitSwitchNormal::Disabled);
 	uptake->config().apply();
-	
+
 	front_intake_solenoid = new Piston(FRONT_PISTON_ID, Piston::PistonType::SINGLE);
 	back_intake_solenoid = new Piston(BACK_PISTON_ID, Piston::PistonType::SINGLE);
 }
@@ -369,20 +413,19 @@ int main(int argc, char **argv)
 
 	ros::Rate rate(100);
 
-	while(ros::ok())
+	while (ros::ok())
 	{
 		ros::spinOnce();
-		
-			
+
 		determineDeployDirection();
-		if(retract_intake || manual_intake || manual_outake)
+		if (retract_intake || manual_intake || manual_outake)
 		{
-			if(retract_intake)
+			if (retract_intake)
 			{
-				front_intake_solenoid-> set(Piston::PistonState::OFF);
-				back_intake_solenoid-> set(Piston::PistonState::OFF);
+				front_intake_solenoid->set(Piston::PistonState::OFF);
+				back_intake_solenoid->set(Piston::PistonState::OFF);
 			}
-			if( manual_intake )
+			if (manual_intake)
 			{
 				front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, 1.0, 0);
 				front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 1.0, 0);
@@ -390,7 +433,7 @@ int main(int argc, char **argv)
 				back_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, 1.0, 0);
 				uptake->set(Motor::Control_Mode::PERCENT_OUTPUT, 0, 0);
 			}
-			if( manual_outake )
+			if (manual_outake)
 			{
 				front_belt->set(Motor::Control_Mode::PERCENT_OUTPUT, -1.0, 0);
 				front_roller->set(Motor::Control_Mode::PERCENT_OUTPUT, -1.0, 0);
@@ -402,15 +445,15 @@ int main(int argc, char **argv)
 		else
 		{
 			stateMachineStep();
-			if( deployed_direction == DeployedDirection::FRONT)
+			if (deployed_direction == DeployedDirection::FRONT)
 			{
-				front_intake_solenoid-> set(Piston::PistonState::ON);
-				back_intake_solenoid-> set(Piston::PistonState::OFF);
+				front_intake_solenoid->set(Piston::PistonState::ON);
+				back_intake_solenoid->set(Piston::PistonState::OFF);
 			}
-			if( deployed_direction == DeployedDirection::BACK)
+			if (deployed_direction == DeployedDirection::BACK)
 			{
-				front_intake_solenoid-> set(Piston::PistonState::OFF);
-				back_intake_solenoid-> set(Piston::PistonState::ON);
+				front_intake_solenoid->set(Piston::PistonState::OFF);
+				back_intake_solenoid->set(Piston::PistonState::ON);
 			}
 		}
 		rate.sleep();
